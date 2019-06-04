589e1beb50ceece566e2e4ce738b9c28a33fdb41
toni-t-t <toni.tanneberger@gmail.com>
2019-06-04 18:03:46 +0200

    Erstes Unity Grundgerüst

---
+++ New Test/Assets/ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void NewGameBtn()
+    {
+        SceneManager.LoadScene("Inselkarte1");
+    }
+
+    public void CloseGame()
+    {
+        Debug.Log("CLOSED");
+        Application.Quit();
+    }
+
+    public void GoToInsel1()
+    {
+        SceneManager.LoadScene("Insel1");
+    }
+
+    public void GoToLevelx(int x)
+    {
+        SceneManager.LoadScene("Level" + x);
+        Debug.Log(x);
+    }
+}
