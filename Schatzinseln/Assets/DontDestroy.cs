87a6f1c5a04d714b1db9ebe09c61c8bd16e42ac6
toni-t-t <toni.tanneberger@gmail.com>
2019-06-05 22:00:10 +0200

    Mute Button mit Platzhalter Musik

---
+++ Schatzinseln/Assets/DontDestroy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    private void Start()
+    {
+        var muteBtn = GetComponent<UnityEngine.UI.Toggle>();
+        if (AudioListener.volume == 1)
+        {
+            muteBtn.isOn = false;
+        }
+        else
+        {
+            muteBtn.isOn = true;
+            musik();
+        }
+    }
+
+    private void Awake()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Musik");
+        if (objects.Length > 1)
+            Destroy(this.gameObject);
+
+        if(this.gameObject.CompareTag("Musik"))
+            DontDestroyOnLoad(this.gameObject);
+    }
+    public void musik()
+    {
+        if (AudioListener.volume == 1)
+            AudioListener.volume = 0;
+        else
+            AudioListener.volume = 1;
+    }
+}
