65ab7f3d5979242eaaedb24a45335e11b0592fff
toni-t-t <toni.tanneberger@gmail.com>
2019-06-06 18:56:28 +0200

    Pyramiden in Progress

---
+++ Schatzinseln/Assets/DragHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject draggedItem;
+    Vector3 startPos;
+    Transform startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+
+    {
+        draggedItem = gameObject;
+        startPos = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        draggedItem = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+        {
+            transform.position = startPos;
+        }
+        
+    }
+
+}
---
+++ Schatzinseln/Assets/Slot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    #region IDropHandler implementation
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            Debug.Log(DragHandler.draggedItem.transform);
+            Debug.Log(transform);
+            if(DragHandler.draggedItem.transform.name.Equals(transform.name))
+            {
+                DragHandler.draggedItem.transform.SetParent(transform);
+                Debug.Log("Richtig!");
+            }
+            else
+            {
+                Debug.Log("Falsch!");
+            }
+            // ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
+        }
+    }
+    #endregion
+}
